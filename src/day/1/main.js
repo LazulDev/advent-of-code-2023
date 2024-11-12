7a6ea9b3929ee904f8ea5d0f4e96a7123552f2c0
María <msanchez.telecom@gmail.com>
2024-11-12 17:39:38 +0100

    feat: algorithm

--- src/day/1/main.js
+++ src/day/1/main.js
@@ -2,12 +2,15 @@ const MOCKED_CALIBRATION_DOCUMENT = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
+const NUMERIC_CHAR_REGEX = '[1-9]'
 function main() {
     // TODO get from assets/day/1/input.txt
     const calibrationDocument = MOCKED_CALIBRATION_DOCUMENT
-    // Get lines
     const lines = calibrationDocument.split('\n')
-    console.log(lines)
+    const firstCharacters = lines.map(line => line.search(NUMERIC_CHAR_REGEX))
+    const lastCharacters = lines.map(line => reverseString(line)).map(line => line.search(NUMERIC_CHAR_REGEX))
+    console.log(firstCharacters)
+    console.log(lastCharacters)
 }
 main()
 
