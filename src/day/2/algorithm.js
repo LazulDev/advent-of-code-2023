56c30825a7f0e2ab4fa9d0bfbc4b4eb4cb053c8e
María Sánchez Carvajal <maria.carvajal@galpsolar.com>
2024-11-13 17:13:20 +0100

    refactor: extract functions to utils folder

--- src/day/2/algorithm.js
+++ src/day/2/algorithm.js
@@ -1,8 +1,8 @@
-import sumArray from "../../utils/sum-array.js";
 import reverseString from "../../utils/reverse-string.js";
+import sumArray from "../../utils/sum-array.js";
 
 const NUMERIC_CHAR_REGEX = '[1-9]'
-export function algorithm(calibrationDocument) {
+export default function algorithm(calibrationDocument) {
     const lines = calibrationDocument.split('\n')
     const firstCharacters = lines
         .map(line => line.at(line.search(NUMERIC_CHAR_REGEX)))
@@ -17,4 +17,3 @@ export function algorithm(calibrationDocument) {
     
     return sumArray(linesDigit.map(str => +str))
 }
-
---
+++ src/utils/sum-array.js
@@ -0,0 +1,7 @@
+export default function sumArray(arr) {
+    let sum = 0
+    arr.forEach(res => {
+        sum += res
+    })
+    return sum
+}
