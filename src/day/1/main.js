ebb67fa4630d5fb2bf8fc1cb383e41fc8fc778e6
María Sánchez Carvajal <maria.carvajal@galpsolar.com>
2024-11-13 17:46:23 +0100

    fix(day-1): fix import

--- src/day/1/main.js
+++ src/day/1/main.js
@@ -1,7 +1,8 @@
 import fs from 'node:fs';
-import { algorithm } from './algorithm';
+import { algorithm } from './algorithm.js';
 
-
+// TODO: parametrizar entrada de main
+// TODO: extraer main a archivo común
 function main() {
     const calibrationDocument = fs.readFileSync('assets/day/1/input.txt', 'utf8')
     const result = algorithm(calibrationDocument)
