53221977b5f3b95d1b7fb03e3c7aa35e77444b2b
María <msanchez.telecom@gmail.com>
2024-11-22 18:34:26 +0100

    test(day-3): add part 2 test case

--- src/day/3/algorithm.test.js
+++ src/day/3/algorithm.test.js
@@ -4,26 +4,32 @@ import algorithm from "./algorithm.js";
 
 describe('algorithm', () => {
     it('part1: should return 4361', () => {
-        const { sut } = setup()
-        const input = `467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..`
+        const { sut, input } = setup()
+       
         const actual = sut(input).part1
         assert.strictEqual(actual, 4361)
     })
 
-    it.todo('part2: should return 467835')
+    it('part2: should return 467835', () => {
+        const { sut, input } = setup()
+       
+        const actual = sut(input).part2
+        assert.strictEqual(actual, 467835)
+    })
 
     const setup = () => {
         return {
-            sut: algorithm
+            sut: algorithm,
+            input:  `467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..`
         }
     }
 })
