cfea9c5c5c79f65a0c83e5a8d6bb3ceaacf7b9c1
María Sánchez Carvajal <maria.carvajal@galpsolar.com>
2024-11-25 13:20:25 +0100

    feat(day-3): part 2 done

--- src/day/3/algorithm.js
+++ src/day/3/algorithm.js
@@ -83,61 +83,74 @@ function isSymbolAdjacent({num, row, col}, input) {
     return false
 }
 /**
- * Return the adjacents position given a gear (symbol *) position
- * @param {*} row and col determine the position of the gear
+ * Return the adjacent numbers' position given a gear (symbol *) position
+ * @param {Array<{number: number, init: number, end: number}>}
  */
-function getNumAdjacentsNumbers({row, col, input}) {
-    const adjacents = [] // {row, col}
-    const width = 1
+function getAdjacentNumbers({ gear: {row, col}, input}) {
     const lines = getLines(input)
 
-
-     // TODO
     const upperRow = lines[row - 1]
-    if (upperRow) {
-        const init = col - 1 >= 0 ?col - 1 : 0
-        const end = col + width + 1 < upperRow.length ? col + width + 1 : upperRow.length
-        const upperSegment = upperRow.slice(init, end)
-        const upperLineHasSymbol = NUM_REGEX.test(upperSegment)
-        if (upperLineHasSymbol) {
-            return true
-        }
-    }
-
+    const sameRow =  lines[row]
     const lowerRow = lines[row + 1]
+
+    const adjacentLines = []
+    if (upperRow) {
+        adjacentLines.push(upperRow)
+    }
+    adjacentLines.push(sameRow)
     if (lowerRow) {
-        const init = col - 1 >= 0 ?col - 1 : 0
-        const end = col + width + 1 < lowerRow.length ? col + width + 1 : lowerRow.length
-        const lowerSegment = lowerRow.slice(init, end)
-        const lowerSegmentHasSymbol = NUM_REGEX.test(lowerSegment)
-        if (lowerSegmentHasSymbol) {
-            return true
-        }
+        adjacentLines.push(lowerRow)
     }
+    return print(adjacentLines
+        .map(line => getLineNumbers(line))
+        .flat()
+        .filter(num => !!num))
+        .filter(num => {
+            const isAdjacent = col >= num.init - 1 && col <= num.end + 1
 
-    const sameRow = lines[row]
+            console.log('number ' + num.number , isAdjacent ?'is adjacent': 'is not adjacent', 'to gear', {row, col})
+            return isAdjacent
+        })
 
-    const leftPos = col - 1
-    if (leftPos > 0) {
-        const leftChar = sameRow.at(leftPos)
-        if (NUM_REGEX.test(leftChar)) {
-            return true
-        }
-    }
-
-    const rightPos = col + numLength + 1 - 1
-    if (rightPos < sameRow.length) {
-        const rightChar = sameRow.at(rightPos)
-
-        if (NUM_REGEX.test(rightChar)) {
-            return true
-        }
-    }
-
-    return adjacents
 }
 
-const input = 
+/**
+ *
+ * @param {string} line - La línea de texto de entrada.
+ * @return {Array<{number: number, init: number, end: number}>} -
+ */
+function getLineNumbers(line) {
+    let number = ''
+    let init = null
+    const lineNumbers = []
+    line.split('').forEach((character, col) => {
+        if (character.match(NUM_REGEX)) {
+            number = number + character
+            init = col - number.length + 1
+
+            const numEnded = init + 1 >= line.length || !NUM_REGEX.test(line[col + 1])
+
+            if (numEnded) {
+                lineNumbers.push({number: Number(number), init, end: init + number.length - 1})
+                number = ''
+                init = null
+            }
+        }
+
+    })
+    console.log({lineNumbers})
+    return lineNumbers
+}
+
+// TODO borrar
+function isAdjacent({number: {init, end, row: numberRow}, gear: {row: gearRow, col: gearCol}}) {
+    if (numberRow > gearRow + 1 || numberRow < gearRow -1) {
+        return false
+    }
+    return gearCol >= init - 1 && gearCol <= end + 1
+}
+
+const input =
 `467..114..
 ...*......
 ..35..633.
@@ -153,17 +166,22 @@ const GEAR_REGEX = new RegExp(/\*/g);
 
 function part2(input) {
     const gears = []
-    const lines = getLines(input).forEach((line, row) => {
+    getLines(input).forEach((line, row) => {
         let match = null
         while ((match = GEAR_REGEX.exec(line)) !== null) {
             gears.push({row, col: match.index})
         }
     })
-    return gears
-        // Set adjacents
-        .map(({row,col}) => ({ row, col, numAdjacents: getNumAdjacentsNumbers({row, col, input })})
-        .filter(({adjacents}) => {})
-    
-    // gears = [{row:0, col:0, adjacents:[]}]
+
+    return print(gears
+        .map((gear) => ({ row: gear.row, col: gear.col, adjacentNumbers: getAdjacentNumbers({ gear, input })}))
+        .filter(({adjacentNumbers}) => adjacentNumbers.length === 2)
+        .map(({adjacentNumbers}) => adjacentNumbers.map(({number}) => number)))
+        .reduce((acc, [a, b]) => a * b, 0)
+}
+
+function print(input) {
+    console.log('print ', input)
+    return input
 }
 
